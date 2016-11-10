8a33be945aa051c6995f4a4d4966a8b90c1c6e17
riri9385 <riri9385@ER02894N.ebgroup.elektrobit.com>
2016-11-10 08:49:43 +0100

    Uploaded First Version

---
+++ src/main/java/com/eso_mibhigh_speech/ebtools/StartController.java
@@ -0,0 +1,25 @@
+package com.eso_mibhigh_speech.ebtools;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+public class StartController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+}
